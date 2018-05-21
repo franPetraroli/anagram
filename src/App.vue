4d5d950e42f611a526ec1617a7d8a6bfdc2489e1
Fra Pet <frapetim@Fras-MacBook-Pro.local>
2018-05-21 15:29:02 +1000

    added bulma

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,63 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+   <section class="hero is-fullheight is-default is-bold" id="app">
+        <div class="hero-head">
+            <nav class="navbar">
+                <div class="container">
+                    <div class="navbar-brand">
+                        <a class="navbar-item" href="../">
+                          <h1>ANAGRAMMA</h1>
+                        </a>
+                        <span class="navbar-burger burger" data-target="navbarMenu">
+            <span></span>
+                        <span></span>
+                        <span></span>
+                        </span>
+                    </div>
+                    <div id="navbarMenu" class="navbar-menu">
+                        <div class="navbar-end">
+                            <div class="tabs is-right">
+                                <ul>
+                                    <li class="is-active"><a>Home</a></li>
+                                    <li><a href="">Saved words</a></li>
+                                    <li><a href="">Log Out</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-6 is-offset-3">
+                    <h1 class="title">
+                        Create an Anagram
+                    </h1>
+                    <h2 class="subtitle">
+                        Enter a word and see all possible combination
+                    </h2>
+
+                    <div class="field is-grouped">
+                        <p class="control is-expanded">
+                            <input class="input" type="text" placeholder="Enter your word" id="word">
+                        </p>
+                        <p class="control">
+                            <a class="button is-info" id="submit">Submit</a>
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <ul id="list"></ul>
+
+        <div class="columns is-centered">
+            <div class="column has-text-centered is-3">
+                <p>Created by <a href="http://franpetraroli.com">Fran Petraroli 2018</a></p>
+            </div>  
+        </div>
+    </section>
 </template>
 
 <script>
@@ -11,13 +66,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+ @import "../node_modules/bulma/bulma.sass";
 </style>
