91fe96c2a4dd3a74488c1dd72c5d1a5ca8978a36
frapet <frapet@francescos-MacBook-Pro.local>
2018-06-02 20:47:38 +1000

    fixed card style

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,80 +1,66 @@
 <template>
   <div class="container">
-    <ul>
-      <li v-for="item in words" :key="words[item]">
-        <div class="item"> 
-          {{ item }}
-        </div>
-        <div class="icons">
-          <i class="far fa-star" id="star"></i>
-          <i class="far fa-times-circle" id="cross" @click="remove(words.indexOf(item))"></i>
-        </div>
-      </li>
-    </ul>
+    <div class="card" v-for="item in words" :key="words[item]">
+      <h5 class="word">{{ item }}</h5>
+      <div class="icons">
+        <i class="far fa-star" id="star"></i>
+        <i class="far fa-times-circle" id="cross" @click="remove(words.indexOf(item))"></i>
+      </div>
+    </div>
+
   </div>
 </template>
 <script>
-import store from '../store'
-  export default {
-    name: 'HelloWorld',
-    data() {
-      return{
-        words: store.state.word_array
-      }
-    },
-    methods: {
-      remove: (itemx) => {
-        store.commit('splice', itemx)
-      }
+import store from "../store";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      words: store.state.word_array
+    };
+  },
+  methods: {
+    remove: itemx => {
+      store.commit("splice", itemx);
     }
   }
-
+};
 </script>
 <style lang="scss" scoped>
-  ul {
-    margin-top: 60px;
-  }
+.container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 20px;
+}
 
-  li {
-    float: left;
-    width: 8em;
-    margin: 10px;
-    padding: 10px 20px;
-    border: 1px solid #ccc;
-    box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.2);
-    text-align: center;
-  }
+.card {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  padding: 10px 20px;
+}
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
 
-  li #star {
-    text-align: right;
-  }
+.word {
+  grid-column: 1 / span 2;
+}
 
-  li:hover {
-    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-  }
+.icons {
+  justify-self: end;
+  align-self: center;
+}
 
-  .items {
-    display: inline;
-    width: 3em;
-  }
+#star,
+#cross {
+  font-size: 18px;
+}
 
-  .icon {
-    display: inline;
-    width: 3em;
-  }
+#star:hover {
+  color: rgb(40, 207, 40);
+}
 
-  #star,
-  #cross {
-    margin-top: 10px;
-    font-size: 18px;
-  }
-
-  #star:hover {
-    color: rgb(40, 207, 40);
-  }
-
-  #cross:hover {
-    color: red;
-  }
-
+#cross:hover {
+  color: red;
+}
 </style>
